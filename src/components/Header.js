525c8c2529148a4f4fd3e8d87ed036ef9fc780da
Your Name <irfana.27@gmail.com>
2023-02-15 01:36:00 -0800

    updated git ignore file

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='text-center'>Quran-quiz</div>
+  )
+}
+
+export default Header
---
+++ src/components/Startpage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Question from './Question'
+
+
+const Startpage = ({startQuiz,data}) => {
+    const[showQuestions,setShowquestions]=useState(false)
+    const handlestartquiz =()=>{
+setShowquestions(true)
+    }
+    console.log(showQuestions,startQuiz);
+  return (
+    <main>
+      {showQuestions ? <Question 
+      data={data}/>:
+        <div className='startPage-main'>
+            <h4>team as-haab</h4>
+            <button className='btn1 start-btn'  onClick={handlestartquiz}>startQuiz</button>
+       </div> 
+        
+    }
+    </main>
+  )
+      
+    
+    
+  
+}
+
+export default Startpage
